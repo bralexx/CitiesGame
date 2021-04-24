c67c55c88981596bc3069170a6f123afeef3c01c
bralexx <bruek.aiu@phystech.edu>
2021-04-25 00:22:00 +0300

    one of many final commits

--- Game.py
+++ Game.py
@@ -1,4 +1,5 @@
 import json
+import flask
 
 
 class Game:
@@ -8,31 +9,31 @@ class Game:
     step_list = []
     player_info = {}
     player_list = []
+
     def __init__(self, name_, player_list_, start_letter):
         self.name = name_
         self.player_list = player_list_
         for player in player_list_:
-            self.player_info.update({player:[0, "in game"]})
+            self.player_info.update({player: [0, "in game"]})
         self.current_letter = start_letter
         self.current_player = 0
         all_cities = json.load(open("config/world-cities_json.json", 'r'))
         self.possible_dictionary = {}
         for city in all_cities:
-            self.possible_dictionary.update({city['name']:city['country']})
+            self.possible_dictionary.update({city['name']: city['country']})
 
-    def __init__(self, data):
-        self.name = data['name']
-        self.status = data['status']
-        self.used_cities = data['used_cities']
-        self.step_list = data['step_list']
-        self.player_info = data['player_info']
-        self.player_list = data['player_list']
-        self.current_letter = data['current_letter']
-        self.current_player = data['current_player']
+    def load_game(data):
+        new_game = Game(data['name'], data['player_list'], data['current_letter'])
+        new_game.status = data['status']
+        new_game.used_cities = data['used_cities']
+        new_game.step_list = data['step_list']
+        new_game.player_info = data['player_info']
+        new_game.current_player = data['current_player']
+        return new_game
 
     def is_step_possible(self, city_name):
         if city_name not in self.used_cities and city_name in self.possible_dictionary and \
-                city_name[0].lower() == self.current_letter:
+                city_name[0].lower() == self.current_letter.lower():
             return True
         return False
 
@@ -42,16 +43,16 @@ class Game:
             self.used_cities.append(city)
             self.player_info[self.player_list[self.current_player]][0] += 1
             self.current_player += 1
-            self.current_letter = city[len(city) - 1]
+            self.current_letter = city[len(city) - 1].upper()
         else:
             if city in self.used_cities:
                 result = "This city has been already said, {} lost".format(self.player_list[self.current_player])
-            elif not city[0].lower() == self.current_letter:
+            elif not city[0].lower() == self.current_letter.lower():
                 result = "Wrong letter, {} lost".format(self.player_list[self.current_player])
             else:
                 result = "This city doesn't exist, {} lost".format(self.player_list[self.current_player])
             self.step_list.append([False, self.player_list[self.current_player], result])
-            self.player_info[self.player_list[self.current_player]][1] = "lost on step {}".format(len(self.step_list) + 1)
+            self.player_info[self.player_list[self.current_player]][1] = "lost on step {}".format(len(self.step_list))
             self.player_list.pop(self.current_player)
         self.current_player %= len(self.player_list)
         self.win_condition()
@@ -59,18 +60,36 @@ class Game:
     def win_condition(self):
         if len(self.player_list) == 1:
             self.status = "ended, winner is {}".format(self.player_list[0])
+            self.step_list.append([False, self.player_list[0], "{} is winner!".format(self.player_list[0])])
 
     def save_game(self):
         data = {}
-        data.update({'name':self.name,
-                     'status':self.status,
-                     'used_cities':self.used_cities,
-                     'step_list':self.step_list,
-                     'player_info':self.player_info,
-                     'player_list':self.player_list,
-                     'current_letter':self.current_letter,
-                     'current_player':self.current_player,
+        data.update({'name': self.name,
+                     'status': self.status,
+                     'used_cities': self.used_cities,
+                     'step_list': self.step_list,
+                     'player_info': self.player_info,
+                     'player_list': self.player_list,
+                     'current_letter': self.current_letter,
+                     'current_player': self.current_player,
                      })
         file = open("games/{}.json".format(self.name), 'w')
-        json.dumps(data, file)
+        data = json.dumps(data)
+        file.write(data)
+        return file.name
 
+    def render_game(self):
+        players_ = []
+        for player in self.player_info:
+            if self.player_info[player][1] == "in game":
+                players_.append([player, "made {} moves and still in game".format(self.player_info[player][0])])
+            else:
+                players_.append([player, self.player_info[player][1]])
+
+        return flask.render_template("game.html",
+                                     game_name=self.name,
+                                     steps=zip(range(len(self.step_list)), self.step_list),
+                                     current_player=self.player_list[self.current_player],
+                                     current_letter=self.current_letter,
+                                     status=self.status,
+                                     players=players_)
--- app.py
+++ app.py
@@ -1,33 +1,74 @@
-from flask import Flask, jsonify, request, redirect
-
-
 import flask
 import json
 import GameManager
 
+app = flask.Flask(__name__)
+GameManager.app = app
 
-app = Flask(__name__)
-from Game import Game
-#g = Game("test", ["1", "2", "3"], 'L')
+current_game = GameManager.Game
 
-next_step = "fnkldsflv;sd"
-all_cities = json.load(open("config/world-cities_json.json", 'r'))
-dictionary = {}
-for city in all_cities:
-    if city['name'][0] in dictionary:
-        dictionary[city['name'][0]] += [{city['name'], city['country']}]
-    else:
-        dictionary.update({city['name'][0]:[{city['name'], city['country']}]})
-
-all_steps = [[1, "name", "country"], [2, "name", "country"]]
 
 @app.route('/')
-def start_game():
-    return flask.render_template('game.html', steps=all_steps, next_step=next_step)
+def start_manager():
+    return GameManager.render_manager()
 
-# @app.route('/make_step', methods=['POST'])
-# def make_step():
-#     global all_steps, next_step
-#     all_steps += [[len(all_steps) + 1, flask.request.form['next_step'], "country"]]
-#     return redirect('/')
+
+@app.route('/open_game_menu', methods=['POST'])
+def open_game_menu():
+    num_of_players = int(flask.request.form['num_of_players'])
+    return flask.render_template('gameMenu.html', num_of_players=num_of_players)
+
+
+@app.route('/create_new_game/<num_of_players>', methods=['POST'])
+def create_new_game(num_of_players):
+    players = []
+    name = flask.request.form['game_name']
+    for i in range(int(num_of_players)):
+        x = flask.request.form['name{}'.format(i)]
+        players.append(flask.request.form['name{}'.format(i)])
+    first_letter = flask.request.form['letter']
+    GameManager.create_game(name, players, first_letter)
+    global current_game
+    current_game = GameManager.get_game(len(GameManager.game_list) - 1)
+    return current_game.render_game()
+
+
+@app.route('/continue_game/<num_of_game>')
+def continue_game(num_of_game):
+    global current_game
+    current_game = GameManager.load_game(int(num_of_game))
+    return current_game.render_game()
+
+
+@app.route('/delete_game/<num_of_game>')
+def delete_game(num_of_game):
+    GameManager.delete_game(int(num_of_game))
+    return GameManager.render_manager()
+
+
+@app.route('/save_current_game')
+def save_current_game():
+    current_game.save_game()
+    return current_game.render_game()
+
+
+@app.route('/save_exit_game')
+def save_exit_game():
+    current_game.save_game()
+    return GameManager.render_manager()
+
+
+@app.route('/exit_game')
+def exit_game():
+    return GameManager.render_manager()
+
+
+@app.route('/make_step', methods=['POST'])
+def make_step():
+    global current_game
+    step = flask.request.form['next_step']
+    current_game.make_step(step)
+    return current_game.render_game()
+
+
 app.run()
